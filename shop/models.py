8e856ecafe34da7d135a3e49a9117e1bab1faa4d
9110ivan2 <ivan2.ivanov91@gmail.com>
2024-08-23 15:15:39 +0200

    feat: creation of models Product, Category, ProductImage

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from user.models import Seller
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    is_new = models.BooleanField(default=False)
+    stock = models.IntegerField()
+    seller = models.ForeignKey(
+        Seller, related_name='seller', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="category", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        Product, related_name="product_images", on_delete=models.CASCADE
+    )
+    image = models.URLField()
+
+    def __str__(self):
+        return self.product.name
