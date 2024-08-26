a07df20e663e8f464a24f0ff64c85b76c5bd1c6f
9110ivan2 <ivan2.ivanov91@gmail.com>
2024-08-26 22:23:26 +0200

    feat: ProductList, ProductDetail, CategoryList, CategoryDetail, ProductImageList, ProductImageDetail class based views created, including authentication added for all but ProductList and ProductDetail views '

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from shop.models import Product, Category, ProductImage
+from shop.serializer import (
+    ProductSerializer,
+    CategorySerializer,
+    ProductImageSerializer,
+)
+from rest_framework import generics
+from rest_framework import permissions
 
-# Create your views here.
+
+## ProductList and ProductDetail views created
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+## CategoryList and CategoryDetail views created
+
+
+class CategoryList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+## ProductImageList and ProductImageDetail views created
+
+
+class ProductImageList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageSerializer
+
+
+class ProductImageDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageSerializer
