9b5cc6e1b120325621ee840dc5d3f5a793a45037
9110ivan2 <ivan2.ivanov91@gmail.com>
2024-08-27 09:17:00 +0200

    fix: permission_classes added for ProductList and Productdetail

--- shop/views.py
+++ shop/views.py
@@ -13,11 +13,13 @@ from rest_framework import permissions
 
 
 class ProductList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
