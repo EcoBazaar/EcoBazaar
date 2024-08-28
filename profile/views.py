6e88755e32d4d1894c3c91529283f108d10b5020
9110ivan2 <ivan2.ivanov91@gmail.com>
2024-08-28 09:26:53 +0200

    feat: cart class based views and cartitem class based views created

--- profile/views.py
+++ profile/views.py
@@ -2,8 +2,8 @@ from rest_framework.response import Response, status
 from rest_framework.views import APIView
 
 
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import generics, serializers, permissions
+
 
 from profile.permission import IsOwnerOrAdmin
 from profile.models import (
@@ -97,3 +97,101 @@ class RegisterView(APIView):
         # Return errors if UserSerializer is invalid
         return Response(user_serializer.errors,
                         status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+## CartList and CartDetail views only authenticated users can access/mofify their cart or superuser
+## get_queryset method filters the cart based on the authenticated user
+## save_user_cart method saves the cart with the authenticated user
+class CartList(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        return Cart.objects.filter(customer=self.request.user)
+
+    def save_user_cart(self, serializer):
+        return serializer.save(customer=self.request.user)
+
+## CartDetail view only authenticated customer of the cart can access/mofify their cart or superuser
+## get_queryset method filters the cart based on the authenticated user
+class CartDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrAdmin]
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        return Cart.objects.filter(customer=self.request.user)
+
+## CartItemList view only authenticated customer of the cart can access/mofify their cart or superuser
+## product_in_cart method checks if the product is in stock and adds it to the cart
+## get_queryset method filters the cart based on the authenticated user and cart id
+class CartItemList(generics.ListCreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+    permission_classes = [IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        cart_id = self.kwargs.get("cart_id")
+        return CartItem.objects.filter(
+            cart__id=cart_id, cart__customer=self.request.user
+        )
+    
+    def product_in_cart(self, serializer):
+        cart_id = self.kwargs.get("cart_id")
+        cart = Cart.objects.get(id=cart_id)
+        product = serializer.validated_data["product"]
+        quantity = serializer.validated_data["quantity"]
+
+        if product.stock < quantity:
+            raise serializers.ValidationError({"message": "Product out of stock"})
+        else:
+            product.stock -= cart.quantity
+            product.save()
+    
+## CartItemDetail view only authenticated customer of the cart can access/mofify their cart or superuser
+## get_queryset method filters the cart based on the authenticated user, cart id and item id
+##
+class CartItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrAdmin]
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        cart_id = self.kwargs.get("cart_id")
+        item_id = self.kwargs.get("pk")
+        return CartItem.objects.filter(
+            cart__id=cart_id, cart__customer=self.request.user, pk=item_id
+        )
+## overriding the update method to update the quantity of the product in the cart
+## if the quantity is less than 0, return a bad request
+## if the quantity is 0, delete the product from the cart
+## if the quantity is greater than 0, update the quantity of the product in the cart
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        quantity = request.data.get("quantity", None)
+
+        if quantity is not None:
+            if quantity < 0:
+                return Response(
+                    {"message": "Quantity cannot be less than 0"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            elif quantity == 0:
+                return self.destroy(request, *args, **kwargs)
+            else:
+                instance.quantity = quantity
+                instance.save()
+                serialaizer = self.get_serializer(instance)
+                return Response(
+                    serialaizer.data, status=status.HTTP_200_OK
+                )
+        return super().update(request, *args, **kwargs)
+
+## method to delete the product from the cart and add the quantity back to the stock
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.product.stock += instance.quantity
+        instance.product.save()
+        return super().destroy(request, *args, **kwargs)
