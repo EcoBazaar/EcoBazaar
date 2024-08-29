2396aa8ecf096bd8f5fc7e13b51e4d49fbcb1ae0
saeideh-eslamian <s.eslamian.70@gmail.com>
2024-08-29 10:34:58 +0200

    style: perform flake8 for profile app files

--- profile/admin.py
+++ profile/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Address, Customer, Seller, Order, OrderItem
+
 
 admin.site.register(Address)
 admin.site.register(Customer)
 admin.site.register(Seller)
 admin.site.register(Order)
 admin.site.register(OrderItem)
-
-
