4a77b8b39fb6fbff7acc2d67bb74ca10ff67d62d
saeideh-eslamian <s.eslamian.70@gmail.com>
2024-08-29 10:40:37 +0200

    style: perform flake8 for shop  app files

--- shop/urls.py
+++ shop/urls.py
@@ -1,21 +1,23 @@
 from django.urls import path
 from shop.views import (
-    ProductList, 
-    ProductDetail, 
-    CategoryList, 
-    CategoryDetail, 
-    ProductImageList, 
-    ProductImageDetail, 
-    ProductSearchView)
+    ProductList,
+    ProductDetail,
+    CategoryList,
+    CategoryDetail,
+    ProductImageList,
+    ProductImageDetail,
+    ProductSearchView
+)
+
 
 urlpatterns = [
     path('products/', ProductList.as_view(), name='products'),
     path('products/<int:id>/', ProductDetail.as_view(), name='product-detail'),
     path('categories/', CategoryList.as_view(), name='categories'),
-    path('categories/<int:id>/', CategoryDetail.as_view(), name='category-detail'),
-    path('images/', ProductImageList.as_view(), name='images'),   #
-    path('images/<int:id>/', ProductImageDetail.as_view(), name='image-detail'),
-    path('search/', ProductSearchView.as_view(), name='search'),  
-    
-    
+    path('categories/<int:id>/',
+         CategoryDetail.as_view(), name='category-detail'),
+    path('images/', ProductImageList.as_view(), name='images'),
+    path('images/<int:id>/',
+         ProductImageDetail.as_view(), name='image-detail'),
+    path('search/', ProductSearchView.as_view(), name='search'),
 ]
