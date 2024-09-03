27f1b7ba067cc42cadd9fa7eb33be8a8a6cee42b
elnarashakarova <elnarashakarova@gmail.com>
2024-09-03 14:08:48 +0200

    Update urls.py

--- profile/urls.py
+++ profile/urls.py
@@ -10,7 +10,6 @@ from profile.views import (
     OrderList,
     OrderDetail,
     CartList,
-
 )
 urlpatterns = [
     path('customers/', CustomerListCreateView.as_view(), name='customers'),
@@ -19,11 +18,14 @@ urlpatterns = [
     path('sellers/', SellerList.as_view(), name='sellers'),
     path('seller/<int:pk>/', SellerDetail.as_view(), name='seller-detail'),
     path('customer/<int:customer_id>/cart/', CartList.as_view(), name='carts'),
-    path('customer/<int:customer_id>/cart/<int:pk>/', CartDetail.as_view(), name='carts-detail'),
-    path('customer/<int:customer_id>/cart-items/', CartItemList.as_view(), name='cart'), # TODO not test yet
+    path('customer/<int:customer_id>/cart/<int:pk>/',
+         CartDetail.as_view(), name='carts-detail'),
+    path('customer/<int:customer_id>/cart-items/',
+         CartItemList.as_view(), name='cart'),
     path('customer/<int:customer_id>/cart-item/<int:pk>/',
-         CartItemDetail.as_view(), name='cart-detail'), # TODO not test yet
-    path('customer/<int:customer_id>/order/<int:cart_id>/', OrderList.as_view(), name='order'),
-    path('customer/<int:customer_id>/order/<int:cart_id>/<int:pk>/', # pk is item id
+         CartItemDetail.as_view(), name='cart-detail'),
+    path('customer/<int:customer_id>/order/<int:cart_id>/',
+         OrderList.as_view(), name='order'),
+    path('customer/<int:customer_id>/order/<int:cart_id>/<int:pk>/',
          OrderDetail.as_view(), name='order-detail'),     
 ]
