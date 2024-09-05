db7a90032984de6fec8d82afa21ed2a41fb14ea3
9110ivan2 <ivan2.ivanov91@gmail.com>
2024-09-05 09:51:15 +0200

    fix:adjusting the db name in the settings file

--- eco_bazaar/settings.py
+++ eco_bazaar/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = "eco_bazaar.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
+        'NAME': os.environ.get('POSTGRES_DB', "ecobazaar_db"),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('POSTGRES_HOST','localhost'),
